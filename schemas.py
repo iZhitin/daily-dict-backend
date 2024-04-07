bc614e951b5b95366b4e5d939d8ee03c98841a37
Ivan Zhitin <IvanZhitin1997@gmail.com>
2024-04-07 20:07:39 +0300

    Changed network in Dockerfile

--- schemas.py
+++ schemas.py
@@ -1,5 +1,6 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+
 
 # Типизация данных
 
@@ -11,6 +12,8 @@ class STaskAdd(BaseModel):
 
 class STask(STaskAdd):
     id: int
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class STaskId(BaseModel):
