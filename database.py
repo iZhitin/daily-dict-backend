c50608c65ffc1b69985087c95955961b84896095
Ivan Zhitin <IvanZhitin1997@gmail.com>
2024-04-21 18:39:30 +0300

    Changes ip address

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ import asyncpg
 # )
 
 # DATABASE_URL = "postgresql://user:password@host:port/database"
-DATABASE_URL = "postgresql+asyncpg://izhitin:Qwerty_386@95.163.231.19:5432/dd_db"
+DATABASE_URL = "postgresql+asyncpg://izhitin:Qwerty_386@0.0.0.0:5432/dd_db"
 
 engine = create_async_engine(DATABASE_URL)
 
